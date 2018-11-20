e3d9f591200abb5df6144cb720518aff84706ae8
Ali Farhadi <a.farhadi@gmail.com>
2018-11-20 01:26:34 +0100

    Updating version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Redex.MixProject do
   def project do
     [
       app: :redex,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()

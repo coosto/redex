c93e4313781cbb82e2418a0851428b05a677919d
Ali Farhadi <a.farhadi@gmail.com>
2019-03-05 22:24:54 +0100

    Adding LINDEX command

--- README.md
+++ README.md
@@ -59,6 +59,7 @@ For now only a small subset of the commands are supported:
 - LPOP
 - LLEN
 - LRANGE
+- LINDEX
 - RPUSH
 - RPOP
 - PUBLISH
---
+++ lib/redex/command/lindex.ex
@@ -0,0 +1,26 @@
+defmodule Redex.Command.LINDEX do
+  use Redex.Command
+
+  def exec([key, index], state = state(db: db)) do
+    String.to_integer(index)
+  rescue
+    ArgumentError -> reply({:error, "ERR value is not an integer or out of range"}, state)
+  else
+    index ->
+      now = System.os_time(:millisecond)
+
+      case :mnesia.dirty_read(:redex, {db, key}) do
+        [{:redex, {^db, ^key}, list, expiry}] when expiry > now and is_list(list) ->
+          Enum.at(list, index)
+
+        [{:redex, {^db, ^key}, _value, expiry}] when expiry > now ->
+          {:error, "WRONGTYPE Operation against a key holding the wrong kind of value"}
+
+        _ ->
+          nil
+      end
+      |> reply(state)
+  end
+
+  def exec(_, state), do: wrong_arg_error("LINDEX") |> reply(state)
+end

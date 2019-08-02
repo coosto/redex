fdd42e4cba6c7933e402a9195d1efcb0f02ae508
Ali Farhadi <a.farhadi@gmail.com>
2019-08-02 13:56:01 +0200

    Optimizing cleaner process

--- lib/redex/cleaner.ex
+++ lib/redex/cleaner.ex
@@ -1,57 +1,44 @@
 defmodule Redex.Cleaner do
-  use Task, restart: :permanent
+  use GenServer
 
   require Logger
 
-  @clean_interval 600_000
-  @lock :redex_cleaner
+  @clean_interval 60_000
+  @name __MODULE__
 
   def start_link([]) do
+    GenServer.start_link(__MODULE__, [], name: @name)
+  end
+
+  def init([]) do
+    {:ok, nil, {:continue, :init}}
+  end
+
+  def handle_continue(:init, state) do
     :mnesia.wait_for_tables([:redex], :infinity)
-    Task.start_link(__MODULE__, :clean, [])
+    :timer.send_interval(@clean_interval, :clean)
+    {:noreply, state}
   end
 
-  def clean do
-    :global.trans(
-      {@lock, self()},
-      fn ->
-        Process.sleep(@clean_interval)
-        do_clean()
-      end,
-      [node() | Node.list()]
-    )
+  def handle_info(:clean, state) do
+    :global.register_name(@name, self())
 
-    clean()
-  end
+    if :global.whereis_name(@name) == self() do
+      Logger.info("cleaning expired cache entries")
+      now = System.os_time(:millisecond)
 
-  defp do_clean do
-    now = System.os_time(:millisecond)
+      deleted =
+        :mnesia.dirty_select(:redex, [
+          {{:redex, :"$1", :"$2", :"$3"}, [{:<, :"$3", now}], [:"$_"]}
+        ])
+        |> Enum.reduce(0, fn object, deleted ->
+          :mnesia.dirty_delete_object(object)
+          deleted + 1
+        end)
 
-    case :mnesia.table_info(:redex, :size) do
-      size when is_integer(size) and size > 0 ->
-        Logger.info("cleaning expired cache entries")
-        first = :mnesia.dirty_first(:redex)
-        deleted = do_clean(now, first, 0)
-        size = :mnesia.table_info(:redex, :size)
-        Logger.info("#{deleted} entries cleaned, remained #{size}")
-
-      _ ->
-        :ok
+      Logger.info("#{deleted} expired entries cleaned")
     end
-  end
 
-  defp do_clean(_now, :"$end_of_table", deleted), do: deleted
-
-  defp do_clean(now, key, deleted) do
-    next = :mnesia.dirty_next(:redex, key)
-
-    case :mnesia.dirty_read(:redex, key) do
-      [object = {:redex, ^key, _value, expiry}] when expiry < now ->
-        :mnesia.dirty_delete_object(object)
-        do_clean(now, next, deleted + 1)
-
-      _ ->
-        do_clean(now, next, deleted)
-    end
+    {:noreply, state}
   end
 end

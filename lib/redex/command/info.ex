fb99763ee2e9f61f009673fbd032dde65ca9cbf8
Ali Farhadi <a.farhadi@gmail.com>
2019-08-02 15:29:05 +0200

    Optimizing INFO command

--- lib/redex/command/info.ex
+++ lib/redex/command/info.ex
@@ -23,45 +23,34 @@ defmodule Redex.Command.INFO do
 
   defp keyspace do
     now = System.os_time(:millisecond)
-    first = Mnesia.dirty_first(:redex)
 
     keyspace =
-      keyspace(now, first, %{})
-      |> Enum.map(fn {db, info} ->
-        "db#{db}:keys=#{info.keys},expires=#{info.expires},avg_ttl=#{info.avg_ttl}\r\n"
+      Mnesia.dirty_select(:redex, [
+        {{:redex, {:"$1", :_}, :_, :"$2"}, [{:>, :"$2", now}], [{{:"$1", :"$2"}}]}
+      ])
+      |> Enum.reduce(%{}, fn {db, expiry}, acc ->
+        stats =
+          acc
+          |> Map.get(db, %{keys: 0, expires: 0, ttl: 0})
+          |> Map.update!(:keys, &(&1 + 1))
+
+        stats =
+          if expiry do
+            stats
+            |> Map.update!(:expires, &(&1 + 1))
+            |> Map.update!(:ttl, &(&1 + expiry - now))
+          else
+            stats
+          end
+
+        Map.put(acc, db, stats)
+      end)
+      |> Enum.map(fn {db, %{keys: keys, expires: expires, ttl: ttl}} ->
+        avg_ttl = div(ttl, expires)
+        "db#{db}:keys=#{keys},expires=#{expires},avg_ttl=#{avg_ttl}\r\n"
       end)
       |> Enum.join()
 
     "# Keyspace\r\n#{keyspace}"
-  end
-
-  defp keyspace(_now, :"$end_of_table", acc), do: acc
-
-  defp keyspace(now, key, acc) do
-    next = Mnesia.dirty_next(:redex, key)
-
-    acc =
-      case Mnesia.dirty_read(:redex, key) do
-        [{:redex, {db, _key}, _value, expiry}] when expiry > now ->
-          acc = Map.put_new(acc, db, %{keys: 0, expires: 0, avg_ttl: 0})
-          acc = update_in(acc[db].keys, &(&1 + 1))
-
-          if expiry do
-            ttl = expiry - now
-            acc = update_in(acc[db].expires, &(&1 + 1))
-
-            update_in(
-              acc[db].avg_ttl,
-              &Float.ceil((&1 * (acc[db].expires - 1) + ttl) / acc[db].expires)
-            )
-          else
-            acc
-          end
-
-        _ ->
-          acc
-      end
-
-    keyspace(now, next, acc)
   end
 end

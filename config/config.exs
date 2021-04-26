815e78d39ee0525e5b0d131725d67506198b0738
alevan <levanallen@gmail.com>
2021-04-26 14:20:42 -0500

    add epmd strat and host names

--- config/config.exs
+++ config/config.exs
@@ -4,22 +4,31 @@ config :redex,
   port: System.get_env("REDEX_PORT", "6379") |> String.to_integer(),
   quorum: System.get_env("REDEX_QUORUM", "1") |> String.to_integer()
 
+web_hosts = ~w(
+  blitz_web@web-beta-us-central1-a-001
+  blitz_web@web-beta-us-central1-a-002
+  blitz_web@web-beta-us-central1-a-003
+  blitz_web@web-beta-us-central1-a-004
+  blitz_web@web-beta-us-central1-a-005
+  blitz_web@web-beta-us-central1-a-006
+  blitz_web@web-beta-us-central1-a-007
+  blitz_web@web-beta-us-central1-a-008
+  blitz_web@web-beta-us-central1-a-009
+  blitz_web@web-beta-us-central1-a-010
+  blitz_web@web-beta-us-central1-a-011
+)a
+
+auth_payments_hosts = ~w(
+  blitz_auth@blitz-auth-001
+  blitz_auth@blitz-auth-002
+  blitz_payments@blitz-payments-001
+)a
+
 config :libcluster, :topologies,
-  k8s: [
-    strategy: Cluster.Strategy.Kubernetes,
-    config: [
-      kubernetes_ip_lookup_mode: :pods,
-      kubernetes_node_basename: "redex"
-    ]
-  ],
-  gossip: [
-    strategy: Cluster.Strategy.Gossip,
-    config: [
-      port: 45892,
-      if_addr: "0.0.0.0",
-      multicast_addr: "230.1.1.251",
-      multicast_ttl: 1
-    ]
-  ]
+     config :libcluster, :topologies,
+     epmd: [
+       strategy: Cluster.Strategy.Epmd,
+       config: [hosts: "HOSTS@GO_HERE"]
+     ]
 
 if Mix.env() == :test, do: import_config("test.exs")

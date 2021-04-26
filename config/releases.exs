921a547a6b29c6dfe7bcc6daa85309744f8bf0cf
alevan <levanallen@gmail.com>
2021-04-26 14:21:33 -0500

    remove gossip and k8s, does not work and not prod

--- config/releases.exs
+++ config/releases.exs
@@ -3,16 +3,3 @@ import Config
 config :redex,
   port: System.get_env("REDEX_PORT", "6379") |> String.to_integer(),
   quorum: System.get_env("REDEX_QUORUM", "1") |> String.to_integer()
-
-config :libcluster, :topologies,
-  k8s: [
-    config: [
-      kubernetes_namespace: System.get_env("REDEX_K8S_NAMESPACE"),
-      kubernetes_selector: System.get_env("REDEX_K8S_SELECTOR")
-    ]
-  ],
-  gossip: [
-    config: [
-      secret: System.get_env("REDEX_GOSSIP_SECRET")
-    ]
-  ]

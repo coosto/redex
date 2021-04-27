5ff1eef049e529ae0ede0dd82fbbbafabc9ff03d
alevan <levanallen@gmail.com>
2021-04-27 12:10:13 -0500

    update app name in hosts names

--- config/config.exs
+++ config/config.exs
@@ -5,23 +5,23 @@ config :redex,
   quorum: System.get_env("REDEX_QUORUM", "1") |> String.to_integer()
 
 web_hosts = ~w(
-  blitz_web@web-beta-us-central1-a-001
-  blitz_web@web-beta-us-central1-a-002
-  blitz_web@web-beta-us-central1-a-003
-  blitz_web@web-beta-us-central1-a-004
-  blitz_web@web-beta-us-central1-a-005
-  blitz_web@web-beta-us-central1-a-006
-  blitz_web@web-beta-us-central1-a-007
-  blitz_web@web-beta-us-central1-a-008
-  blitz_web@web-beta-us-central1-a-009
-  blitz_web@web-beta-us-central1-a-010
-  blitz_web@web-beta-us-central1-a-011
+  redex@web-beta-us-central1-a-001
+  redex@web-beta-us-central1-a-002
+  redex@web-beta-us-central1-a-003
+  redex@web-beta-us-central1-a-004
+  redex@web-beta-us-central1-a-005
+  redex@web-beta-us-central1-a-006
+  redex@web-beta-us-central1-a-007
+  redex@web-beta-us-central1-a-008
+  redex@web-beta-us-central1-a-009
+  redex@web-beta-us-central1-a-010
+  redex@web-beta-us-central1-a-011
 )a
 
 auth_payments_hosts = ~w(
-  blitz_auth@blitz-auth-001
-  blitz_auth@blitz-auth-002
-  blitz_payments@blitz-payments-001
+  redex@blitz-auth-001
+  redex@blitz-auth-002
+  redex@blitz-payments-001
 )a
 
 config :libcluster, :topologies,
